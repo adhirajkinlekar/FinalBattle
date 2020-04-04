3264ef514a8c88e39460a9929ea579cc9757911a
adhirajkinlekar <adhirajkinlekar123@gmail.com>
2020-04-04 22:29:02 +0530

    Updates

---
+++ precache-manifest.dbaf224a9774f34356842005054af15f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebee30897d40d8628ba9db46ee7fb2bb",
+    "url": "/FinalBattle/index.html"
+  },
+  {
+    "revision": "b98150fadcfd6a507dac",
+    "url": "/FinalBattle/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "2aa0f9b92bd503db9577",
+    "url": "/FinalBattle/static/css/main.5f0be0ec.chunk.css"
+  },
+  {
+    "revision": "b98150fadcfd6a507dac",
+    "url": "/FinalBattle/static/js/2.8392b1c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FinalBattle/static/js/2.8392b1c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa0f9b92bd503db9577",
+    "url": "/FinalBattle/static/js/main.d360baf4.chunk.js"
+  },
+  {
+    "revision": "80d46ef2758b093c5479",
+    "url": "/FinalBattle/static/js/runtime-main.cbee7d91.js"
+  }
+]);
